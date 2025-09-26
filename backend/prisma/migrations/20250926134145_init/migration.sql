2346a24db2df1327335643c02a9f036c3c8fda4d
Infinity <wemersoncontas22@gmail.com>
2025-09-26 10:44:54 -0300

    feat: integrate Prisma and set up initial database schema

---
+++ backend/prisma/migrations/20250926134145_init/migration.sql
@@ -0,0 +1,36 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('USER', 'MODERATOR', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "role" "public"."Role" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Book" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "synopsis" TEXT,
+    "isbn" TEXT,
+    "publicationYear" INTEGER,
+    "pageCount" INTEGER,
+    "coverUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Book_isbn_key" ON "public"."Book"("isbn");
